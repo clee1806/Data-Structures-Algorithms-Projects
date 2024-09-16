57868f698edf34c948857a7cfdf8c122ea27cd62
chelsea <163939469+clee1806@users.noreply.github.com>
2024-09-16 14:26:13 -0700

    Add files via upload

---
+++ Shortest Paths Graphs/ToposortDAGSolver.java
@@ -0,0 +1,83 @@
+package graphs.shortestpaths;
+
+import graphs.Edge;
+import graphs.Graph;
+import minpq.DoubleMapMinPQ;
+import minpq.MinPQ;
+
+import java.util.*;
+
+/**
+ * Topological sorting implementation of the {@link ShortestPathSolver} interface for <b>directed acyclic graphs</b>.
+ *
+ * @param <V> the type of vertices.
+ * @see ShortestPathSolver
+ */
+public class ToposortDAGSolver<V> implements ShortestPathSolver<V> {
+    private final Map<V, Edge<V>> edgeTo;
+    private final Map<V, Double> distTo;
+
+    /**
+     * Constructs a new instance by executing the toposort-DAG-shortest-paths algorithm on the graph from the start.
+     *
+     * @param graph the input graph.
+     * @param start the start vertex.
+     */
+    public ToposortDAGSolver(Graph<V> graph, V start) {
+        edgeTo = new HashMap<>();
+        distTo = new HashMap<>();
+        // TODO: Replace with your code
+        MinPQ<V> perimeter = new DoubleMapMinPQ<>();
+        perimeter.add(start, 0.0);
+        edgeTo.put(start, null);
+        distTo.put(start, 0.0);
+        while (!perimeter.isEmpty()) {
+            V from = perimeter.removeMin();
+            for (Edge<V> e : graph.neighbors(from)) {
+                V to = e.to;
+                double oldDist = distTo.getOrDefault(to, Double.POSITIVE_INFINITY);
+                double newDist = distTo.get(from) + e.weight;
+                if (newDist < oldDist) {
+                    edgeTo.put(to, e);
+                    distTo.put(to, newDist);
+                    perimeter.addOrChangePriority(to, newDist);
+                }
+            }
+        }
+    }
+
+    /**
+     * Recursively adds nodes from the graph to the result in DFS postorder from the start vertex.
+     *
+     * @param graph   the input graph.
+     * @param start   the start vertex.
+     * @param visited the set of visited vertices.
+     * @param result  the destination for adding nodes.
+     */
+    private void dfsPostOrder(Graph<V> graph, V start, Set<V> visited, List<V> result) {
+        // TODO: Replace with your codeD
+        visited.add(start);
+        result.add(start);
+        for (Edge<V> edge: graph.neighbors(start)) {
+            V to = edge.to;
+            if (!visited.contains(to)) {
+                result.add(to);
+                dfsPostOrder(graph, to, visited, result);
+            }
+        }
+        Collections.reverse(result);
+    }
+
+    @Override
+    public List<V> solution(V goal) {
+        List<V> path = new ArrayList<>();
+        V curr = goal;
+        path.add(curr);
+        while (edgeTo.get(curr) != null) {
+            curr = edgeTo.get(curr).from;
+            path.add(curr);
+        }
+        Collections.reverse(path);
+        return path;
+    }
+}
